90f66fb4d2fbe63d29ec57ed095938c582b0dcbb
larat <jocelyn.larat@gmail.com>
2016-09-07 16:44:50 +0200

    HUUUUUGE modif - do not touch.. or not too many

--- OProject/webapp/js/app.js
+++ OProject/webapp/js/app.js
@@ -14,34 +14,29 @@ app.config(
         $stateProvider
             .state('index', {
                 url: "/index",
-                templateUrl: "partials/section-connexion.html",
+                templateUrl: "partials/section-overview.html",
                 controller: 'sectionConnectionController'
-            })
-            .state('home', {
-                url: "/home",
-                templateUrl: "partials/section-home.html",
-                controller: 'sectionHomeController'
             })
             .state('overview', {
                 url: "/overview",
                 templateUrl: "partials/section-overview.html",
-                controller: 'sectionOverviewController'
+                controller: 'sectionConnectionController'
             })
             .state('demande', {
                 url: "/demande",
-                templateUrl: "partials/section-demande.html"
+                templateUrl: "partials/section-demande.html",
+                controller: 'sectionDemandeController'
             })
             .state('facture', {
                 url: "/facture",
                 templateUrl: "partials/section-facture.html"
             })
             .state('regulation', {
-                url: "/fils1",
-                templateUrl: "partials/section-facture.html"
+                url: "/regulation",
+                templateUrl: "partials/section-regulation.html"
             })
             .state('impaye', {
                 url: "/impaye",
                 templateUrl: "partials/section-impaye.html"
-            });
-
+            })
     });
--- OProject/webapp/js/controller.js
+++ OProject/webapp/js/controller.js
@@ -3,61 +3,101 @@
  */
 
 app.controller('mainController', function ($scope, $location) {
-
-});
-
-app.controller('sectionConnectionController', function($scope, $location){
-    $scope.connexionLogin = "";
-    $scope.connexionPassword = "";
-
-    $scope.connectionHome = function(){
-
-        $("#containerConnexion").addClass('animated fadeOutDown').delay(1000).then($location.path('/home'));
-    };
-});
-
-app.controller('sectionOverviewController', function($scope, $location){
     $scope.nbDemande = 26;
     $scope.nbFacture = 12;
     $scope.nbRegulation = 20;
     $scope.nbImpaye = 4;
-    $scope.DetailsDemande = function(){
+
+});
+app.controller('sectionHomeController', function ($scope, $state) {
+
+});
+
+app.controller('sectionConnectionController', function ($scope, $location) {
+    $scope.connexionLogin = "";
+    $scope.connexionPassword = "";
+
+    $scope.connectionHome = function () {
+
+        $("#containerConnexion").addClass('animated fadeOutDown').delay(1000).then($location.state('/home.overview'));
+    };
+});
+
+app.controller('sectionOverviewController', function ($scope, $location) {
+
+    $scope.DetailsDemande = function () {
         $location.path('/demande');
     };
 
-    $scope.DetailsFacture = function(){
+    $scope.DetailsFacture = function () {
         $location.path('/facture');
     };
 
-    $scope.DetailsRegulation = function(){
+    $scope.DetailsRegulation = function () {
         $location.path('/regulation');
     };
 
-    $scope.DetailsImpaye = function(){
+    $scope.DetailsImpaye = function () {
         $location.path('/impaye');
     };
 });
 
-app.controller('sectionDemandeController', function($scope, $location){
-    $scope.backOverview = function(){
+app.controller('sectionDemandeController', function ($scope, $location) {
+
+    $scope.demandes = [];
+    $scope.demande = {};
+    $scope.demande1 = {
+        "id": 1,
+        "libelle": "1ere demande",
+        "actif": true
+    };
+    $scope.demande2 = {
+        "id": 2,
+        "libelle": "2e demande",
+        "actif": true
+    };
+    $scope.demande3 = {
+        "id": 3,
+        "libelle": "3e demande",
+        "actif": false
+    };
+    $scope.demandes.push($scope.demande1, $scope.demande2, $scope.demande3);
+
+    $scope.saveDemande = function () {
+        $scope.demandes.push({
+                "id": $scope.demandes.length + 1,
+                "libelle": $scope.demande.libelle,
+                "actif": $scope.demande.actif
+            }
+        );
+        toastr.success("Demande ajout&eacute;e");
+    }
+
+    $scope.editDemande = function(d){
+        $scope.demande.id = d.id;
+        $scope.demande.libelle = d.libelle;
+        $scope.demande.actif = d.actif;
+    };
+
+    $scope.backOverview = function () {
         $location.path('/overview');
     };
 });
 
-app.controller('sectionFactureController', function($scope, $location){
-    $scope.backOverview = function(){
+app.controller('sectionFactureController', function ($scope, $location) {
+    $scope.backOverview = function () {
         $location.path('/overview');
     };
 });
 
-app.controller('sectionRegulationController', function($scope, $location){
-    $scope.backOverview = function(){
+app.controller('sectionRegulationController', function ($scope, $location) {
+    $scope.backOverview = function () {
         $location.path('/overview');
     };
 });
 
-app.controller('sectionImpayeController', function($scope, $location){
-    $scope.backOverview = function(){
+app.controller('sectionImpayeController', function ($scope, $location) {
+    $scope.backOverview = function () {
         $location.path('/overview');
     };
 });
